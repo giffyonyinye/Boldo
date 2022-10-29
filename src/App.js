57fd45666c2d8e93c30d61d494a70ab16fedbb2e
onyinyechi <74349914+giffyonyinye@users.noreply.github.com>
2022-10-29 10:46:11 +0100

    clean up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import "./App.css";
 import HomePage from "./Components/LandingPage/HomePage";
-// import SecondSection from "./Componets/SecondSection";
-// import MiddleSection from "./Componets/MiddleSection";
-// import ThirdSection from "./Componets/ThirdSection";
-// import ForthSection from "./Componets/ForthSection";
-// import SubForth from "./Componets/SubForth";
-// import Contact from "./Componets/Contact";
-// import Footer from "./Componets/Layout/Footer";
+
 import BaseLayout from "./Components/Layout/BaseLayout";
 
 function App() {
@@ -15,15 +9,7 @@ function App() {
       <BaseLayout>
         <HomePage/>
       </BaseLayout>
-      {/* <NavBar />
-      <FirstSection />
-      <SecondSection />
-      <MiddleSection />
-      <ThirdSection />
-      <ForthSection />
-      <SubForth />
-      <Contact />
-      <Footer/> */}
+      
     </div>
   );
 }
